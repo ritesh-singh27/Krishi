1619b797a324c5ea396b93035b4934d73741acd6
ritesh-singh27 <riteshsingh2707@gmail.com>
2024-02-13 18:30:56 +0530

    first commit

---
+++ src/components/preheader/preheader.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./preheader.css";
+
+const PreHeader = () => {
+  const width = window.innerWidth;
+  console.log(width);
+  return (
+    <div className="bg-[#219653] px-6 py-2 md:inline-block hidden w-full">
+      <div className="flex justify-between items-center ml-6">
+        
+        <div className="">
+          <div className="" id="google_element"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PreHeader;
